f524c151ecab64dee4b4cd2073d04887b19ff5b6
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-02-07 20:45:00 -0300

    :tada: Frontend's initial structure

---
+++ frontend/README.md
@@ -0,0 +1,8 @@
+# Frontend for auth-app
+
+Steps to run this project:
+
+1. Run `npm i` command
+3. Run `npm start` or `npm dev` command
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ frontend/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>auth-app</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>
+        Hello World!
+      </h1>
+    </div>
+  )
+}
