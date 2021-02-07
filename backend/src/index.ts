ccd02feda5f3e9c1ecdeafc3ceb33f977c747afb
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-02-07 01:19:31 -0300

    :heavy_plus_sign: chore: add morgan and env variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,18 @@
 import "reflect-metadata";
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import morgan = require("morgan");
+require('dotenv').config()
 
 import routes from "./routes";
 import './database';
 
 const app = express();
 app.use(bodyParser.json());
+app.use(morgan("dev"))
 
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.API_PORT || 3333, () => {
   console.log("🔥 Server is running!");
 });
