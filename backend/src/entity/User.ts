daaa851a7ae05218573d575e0e1ba07e98d65d1b
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-02-06 23:16:23 -0300

    :sparkles: feat: add user entity
    - moving @types/express to devDependencies

---
+++ backend/src/entity/User.ts
@@ -0,0 +1,40 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BeforeInsert,
+  BeforeUpdate,
+} from "typeorm";
+
+import { hashSync } from "bcryptjs";
+
+@Entity()
+export class User {
+  
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword() {
+    this.password = hashSync(this.password);
+  }
+
+}
