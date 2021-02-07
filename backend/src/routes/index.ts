417d5f091bda97690daf432b9219accc0ed89726
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-02-06 23:28:55 -0300

    :lock: feat(backend): add error middleware

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,32 @@
-import { Request, Response, Router } from "express";
+import { Router, Request, Response, NextFunction } from "express";
+import { HttpError } from "http-errors";
+import createHttpError = require("http-errors");
 
 const routes = Router();
 
 routes.get("/", (request: Request, response: Response) => {
   return response.status(200).json({
     message: "Hello world!",
+  });
+});
+
+routes.use((request: Request, response: Response, next: NextFunction) => {
+  const error = new createHttpError.NotFound();
+  next(error);
+});
+
+routes.use(function (
+  error: HttpError,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  response.status(error.status);
+  response.json({
+    error: {
+      status: error.status,
+      message: error.message,
+    },
   });
 });
 
