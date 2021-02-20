349a3a3515d7bf33ed9a26fc53dc213a63799c8b
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-02-20 19:17:00 -0300

    :heavy_plus_sign: chore(frontend): add env variable for api key

--- frontend/api/api.ts
+++ frontend/api/api.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+require("dotenv").config();
+
 export const api = axios.create({
   baseURL: "http://localhost:3333",
+  headers: {
+    "x-api-key": process.env.API_KEY,
+  },
 });
