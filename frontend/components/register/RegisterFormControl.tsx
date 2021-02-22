34e645ca6d4372dff2846c0ddc4a4803f321ad5a
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-02-22 19:12:07 -0300

    :children_crossing: feat(frontend): add form validation
    removed InputGroup component due to validation, will be added again later

--- frontend/components/register/RegisterFormControl.tsx
+++ frontend/components/register/RegisterFormControl.tsx
@@ -1,19 +1,98 @@
 import React from "react";
-import { Stack, Button } from "@chakra-ui/react";
+import {
+  Stack,
+  InputGroup,
+  Input,
+  Button,
+  Icon,
+  InputLeftElement,
+  FormControl,
+} from "@chakra-ui/react";
 import { EmailIcon, LockIcon } from "@chakra-ui/icons";
 import { FaUserAlt } from "react-icons/fa";
 
-import { InputGroup } from "../form/InputGroup";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+interface RegisterFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const schema = yup.object().shape({
+  name: yup.string().required("Name is required").min(5).max(50),
+  email: yup.string().required("Email is required").email(),
+  password: yup.string().required("Password is required").min(8).max(50),
+});
 
 export function RegisterFormControl() {
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: RegisterFormData, e: any) => {
+    console.log(data);
+    e.target.reset();
+  };
+
   return (
-    <Stack spacing={4}>
-      <InputGroup type="text" placeholder="Name" icon={FaUserAlt} />
-      <InputGroup type="email" placeholder="Email" icon={EmailIcon} />
-      <InputGroup type="password" placeholder="Password" icon={LockIcon} />
-      <Button size="md" colorScheme="messenger">
-        Start coding now
-      </Button>
-    </Stack>
+    <form onSubmit={handleSubmit((data, event) => onSubmit(data, event))}>
+      <Stack>
+        <FormControl isInvalid={!!errors.name}>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<Icon as={FaUserAlt} color="gray.500" />}
+            />
+            <Input
+              placeholder="Name"
+              borderColor="gray.300"
+              name="name"
+              id="name"
+              ref={register}
+            />
+          </InputGroup>
+        </FormControl>
+
+        <FormControl isInvalid={!!errors.email}>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<Icon as={EmailIcon} color="gray.500" />}
+            />
+            <Input
+              placeholder="Email"
+              borderColor="gray.300"
+              name="email"
+              id="email"
+              ref={register}
+            />
+          </InputGroup>
+        </FormControl>
+
+        <FormControl isInvalid={!!errors.password}>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<Icon as={LockIcon} color="gray.500" />}
+            />
+            <Input
+              placeholder="Password"
+              borderColor="gray.300"
+              name="password"
+              id="password"
+              ref={register}
+              type="password"
+            />
+          </InputGroup>
+        </FormControl>
+
+        <Button size="md" colorScheme="messenger" type="submit">
+          Start coding now
+        </Button>
+      </Stack>
+    </form>
   );
 }
