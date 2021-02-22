994cbc1c2b69e01f0c01e481e9a42600c6f22eb4
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-02-22 16:56:37 -0300

    :wrench: chore(frontend): add next config, remove dotenv

--- frontend/api/api.ts
+++ frontend/api/api.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-require("dotenv").config();
-
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: process.env.API_URL,
   headers: {
-    "x-api-key": process.env.API_KEY,
+    "X-API-Key": process.env.API_KEY,
   },
 });
