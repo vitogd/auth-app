83b858a26af12753a4419d21ec0fc586c073955a
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-02-22 15:24:21 -0300

    :lock: feat(backend): add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,13 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import morgan = require("morgan");
 require("dotenv").config();
+const cors = require("cors");
 
 import routes from "./routes";
 import "./database";
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 
